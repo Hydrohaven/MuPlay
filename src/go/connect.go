9c88f8ef2a27035bc1327cb2cf1cc466fc97d0d0
Hydrohaven <hydrohaven2004@gmail.com>
2024-12-30 03:50:35 -0800

    Added FastAPI GET requests + Go GET method

--- src/go/connect.go
+++ src/go/connect.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getIntent() string {
+	response, err := http.Get("http://127.0.0.1:8000/get")
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close() // defer causes deferred functions to be called at the end in lifo order, so cool
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return ""
+	}
+
+	var data map[string]string
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error reading JSON: ", err)
+		return ""
+	}
+
+	intent, exists := data["intent"]
+	if exists {
+		fmt.Println(intent)
+	}
+
+	return intent
+}
